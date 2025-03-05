33ce6009d270300b997a4bb79e597882bad62b6e
age-willett <age.willett@gmail.com>
2025-03-05 11:38:58 -0500

    Added textnode, basic test

---
+++ src/textnode.py
@@ -0,0 +1,23 @@
+from enum import Enum
+
+class TextType(Enum):
+    BLOCK = "block"
+    NORMAL = "normal"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
+
+class TextNode():
+    def __init__(self, text: str, text_type: TextType, url: str=None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, o):
+        return self.text == o.text and self.text_type == o.text_type and self.url == o.url
+
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
