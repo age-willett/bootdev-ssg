fe08e7cc85deebd09399974f48a2b7c69ff879ce
age-willett <age.willett@gmail.com>
2025-03-05 17:51:43 -0500

    Added TextNode conversion

--- src/textnode.py
+++ src/textnode.py
@@ -1,8 +1,9 @@
 from enum import Enum
+from leafnode import LeafNode
 
 
 class TextType(Enum):
-    BLOCK = "block"
+    TEXT = "text"
     NORMAL = "normal"
     BOLD = "bold"
     ITALIC = "italic"
@@ -24,3 +25,24 @@ class TextNode:
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+    def text_node_to_html_node(text_node: "TextNode"):
+        match text_node.text_type:
+            case TextType.TEXT:
+                return LeafNode(value=text_node.text)
+            case TextType.BOLD:
+                return LeafNode(tag="b", value=text_node.text)
+            case TextType.ITALIC:
+                return LeafNode(tag="i", value=text_node.text)
+            case TextType.CODE:
+                return LeafNode(tag="code", value=text_node.text)
+            case TextType.LINK:
+                return LeafNode(
+                    tag="a", value=text_node.text, props={"href": text_node.url}
+                )
+            case TextType.IMAGE:
+                return LeafNode(
+                    tag="img",
+                    value="",
+                    props={"src": text_node.url, "alt": text_node.text},
+                )
