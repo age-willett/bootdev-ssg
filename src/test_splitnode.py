131057fb1fffa8fc52b91746c7bbf00574840d8a
age-willett <age.willett@gmail.com>
2025-03-10 11:34:13 -0400

    Added text_to_textnodes, test

--- src/test_splitnode.py
+++ src/test_splitnode.py
@@ -5,6 +5,7 @@ from textnode import (
     split_nodes_delimiter,
     split_nodes_image,
     split_nodes_links,
+    text_to_textnodes,
 )
 
 
@@ -69,4 +70,26 @@ class TestSplitNode(unittest.TestCase):
                 ),
             ],
             new_nodes,
+        )
+
+    def test_text_to_textnodes(self):
+        nodes = text_to_textnodes(
+            "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        )
+        self.assertListEqual(
+            nodes,
+            [
+                TextNode("This is ", TextType.TEXT),
+                TextNode("text", TextType.BOLD),
+                TextNode(" with an ", TextType.TEXT),
+                TextNode("italic", TextType.ITALIC),
+                TextNode(" word and a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" and an ", TextType.TEXT),
+                TextNode(
+                    "obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"
+                ),
+                TextNode(" and a ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://boot.dev"),
+            ],
         )
--- src/textnode.py
+++ src/textnode.py
@@ -58,25 +58,33 @@ def _re_split_by_ends(text, start_block, end_block):
 def split_nodes_delimiter(old_nodes: [TextNode], delimiter: str, text_type: TextType) -> [TextNode]:
     new_nodes = []
     for node in old_nodes:
-        new_nodes.extend(split_node(node, delimiter, delimiter, text_type))
+        if node.text_type == TextType.TEXT:
+            new_nodes.extend(split_node(node, delimiter, delimiter, text_type))
+        else:
+            new_nodes.extend([node])
+
     return new_nodes
 
 def split_node(node: TextNode, start_block: str, end_block: str, text_type: TextType):
+    node_type = node.text_type
+    node_url = node.url
     split_text = _re_split_by_ends(node.text, start_block, end_block)
     for i in range(0, len(split_text), 2):
-        split_text[i] = TextNode(split_text[i], TextType.TEXT)
+        split_text[i] = TextNode(split_text[i], node_type)
     for i in range(1, len(split_text), 2):
-        split_text[i] = TextNode(split_text[i], text_type)
+        split_text[i] = TextNode(split_text[i], text_type, node_url)
     return [new_node for new_node in split_text if new_node.text != ""]
 
 def split_nodes_image(old_nodes: [TextNode]):
     def inner_split_node(node: TextNode):
+        node_url = node.url
+        node_type = node.text_type
         node_text = node.text
         images_in_text = extract_markdown_images(node_text)
         image_nodes = [*map(lambda t: TextNode(t[0], TextType.IMAGE, t[1]), images_in_text)]
         split_text = re.split(r"!\[(?:.*?)\]\((?:.*?)\)", node_text)
         split_text = [*filter(lambda s: s != "", split_text)]
-        text_nodes = [*map(lambda s: TextNode(s, TextType.TEXT), split_text)]
+        text_nodes = [*map(lambda s: TextNode(s, node_type, node_url), split_text)]
         return [val for t in zip_longest(text_nodes, image_nodes) for val in t if val is not None]
 
     new_nodes = []
@@ -86,15 +94,26 @@ def split_nodes_image(old_nodes: [TextNode]):
 
 def split_nodes_links(old_nodes: [TextNode]):
     def inner_split_node(node: TextNode):
+        node_url = node.url
+        node_type = node.text_type
         node_text = node.text
         links_in_text = extract_markdown_links(node_text)
         link_nodes = [*map(lambda t: TextNode(t[0], TextType.LINK, t[1]), links_in_text)]
         split_text = re.split(r"(?<!!)\[(?:.*?)\]\((?:.*?)\)", node_text)
         split_text = [*filter(lambda s: s != "", split_text)]
-        text_nodes = [*map(lambda s: TextNode(s, TextType.TEXT), split_text)]
+        text_nodes = [*map(lambda s: TextNode(s, node_type, node_url), split_text)]
         return [val for t in zip_longest(text_nodes, link_nodes) for val in t if val is not None]
 
     new_nodes = []
     for node in old_nodes:
         new_nodes.extend(inner_split_node(node))
+    return new_nodes
+
+def text_to_textnodes(text):
+    text_node = [TextNode(text, TextType.TEXT)]
+    new_nodes = split_nodes_image(text_node)
+    new_nodes = split_nodes_links(new_nodes)
+    new_nodes = split_nodes_delimiter(new_nodes, "**", TextType.BOLD)
+    new_nodes = split_nodes_delimiter(new_nodes, "_", TextType.ITALIC)
+    new_nodes = split_nodes_delimiter(new_nodes, "`", TextType.CODE)
     return new_nodes
