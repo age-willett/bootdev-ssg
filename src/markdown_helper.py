264e280b78e331470600d9c214468ef3e8d2a742
age-willett <age.willett@gmail.com>
2025-03-10 19:26:24 -0400

    Added block evaluator and tests

--- src/markdown_helper.py
+++ src/markdown_helper.py
@@ -1,13 +1,43 @@
 import re
+from enum import Enum
+
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDEREDLIST = "unordered_list"
+    ORDEREDLIST = "ordered_list"
+
 
 def extract_markdown_images(text: str) -> [tuple]:
     return re.findall(r"!\[(.*?)\]\((.*?)\)", text)
 
+
 def extract_markdown_links(text: str) -> [tuple]:
     return re.findall(r"(?<!!)\[(.*?)\]\((.*?)\)", text)
+
 
 def markdown_to_blocks(markdown: str):
     blocks = markdown.split("\n\n")
     trimmed_blocks = [*map(lambda s: s.strip(), blocks)]
     cleaned_blocks = [*filter(lambda s: s != "", trimmed_blocks)]
     return cleaned_blocks
+
+
+def block_to_block_type(markdown_block: str):
+    match markdown_block:
+        case str(x) if x.startswith(("# ", "## ", "### ", "#### ", "##### ", "###### ")):
+            return BlockType.HEADING
+        case str(x) if x.startswith("```") and x.endswith("```"):
+            return BlockType.CODE
+        case str(x) if x.startswith(">"):
+            return BlockType.QUOTE
+        case str(x) if x.startswith("- "):
+            return BlockType.UNORDEREDLIST
+        case str(x) if x.startswith("1. "):
+            return BlockType.ORDEREDLIST
+        case _:
+            return BlockType.PARAGRAPH
+
--- src/test_splitblocks.py
+++ src/test_splitblocks.py
@@ -1,5 +1,5 @@
 import unittest
-from markdown_helper import markdown_to_blocks
+from markdown_helper import markdown_to_blocks, block_to_block_type, BlockType
 
 class TestSplitBlocks(unittest.TestCase):
     def test_markdown_to_blocks(self):
@@ -89,3 +89,38 @@ With an extra line
                 "This is the second block\nWith an extra line",
             ],
         )
+
+    def test_blocktype_paragraph(self):
+        block = "This is a paragraph"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+
+
+    def test_blocktype_heading(self):
+        for i in range(6):
+            heading_hash = "#" * (i+1) + " "
+            block = f"{heading_hash}This is a heading"
+            self.assertEqual(block_to_block_type(block), BlockType.HEADING)
+        block = "####### This is not a heading"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+
+    def test_blocktype_code(self):
+        block = "```This is a code```"
+        self.assertEqual(block_to_block_type(block), BlockType.CODE)
+        block = "```This is not code"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+
+    def test_blocktype_quote(self):
+        block = ">This is a quote"
+        self.assertEqual(block_to_block_type(block), BlockType.QUOTE)
+
+    def test_blocktype_unordered_list(self):
+        block = "- This is a unordered list"
+        self.assertEqual(block_to_block_type(block), BlockType.UNORDEREDLIST)
+        block = "-This is not an unordered list"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+
+    def test_blocktype_ordered_list(self):
+        block = "1. This is a ordered list"
+        self.assertEqual(block_to_block_type(block), BlockType.ORDEREDLIST)
+        block = "1.This is not an ordered list"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
